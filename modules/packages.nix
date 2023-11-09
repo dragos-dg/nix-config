71650080cdde06be5eb24f945def229612ab91d7
Dragos Dumitrescu <dragos.dumitrescu@chase.io>
2023-11-09 10:50:25 +0000

    added git programs

--- modules/packages.nix
+++ modules/packages.nix
@@ -8,6 +8,7 @@ let
     cargo
     nerdfonts
     utm
+    delta
     hack-font
   ];
 in packages
--- modules/programs.nix
+++ modules/programs.nix
@@ -12,5 +12,31 @@
     enableZshIntegration = true;
     nix-direnv.enable = true;
   };
+  git = {
+    enable = true;
+    extraConfig = {
+      core = {
+        whitespace = "trailing-space,space-before-tab";
+        editor = "nvim";
+        pager = "delta";
+      };
+      interactive = {
+        diffFilter = "delta delta --color-only";
+      };
+      delta = {
+        navigate = true;
+        light = false;
+        side-by-side = true;
+        line-numbers = true;
+        syntax-theme = "Nord";
+        true-color = "always";
+      };
+      merge = {
+        conflictstyle = "diff3";
+      };
+      diff = {
+        colorMoved = "default";
+      };
+    };
+  };
 }
-
